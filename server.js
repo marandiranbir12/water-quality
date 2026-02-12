eb11eb7a353dd792bbf3746f681bc576c18c4832
marandiranbir12 <marandiranbir12@gmail.com>
2026-02-13 00:56:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,18 +3,22 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/api/data", (req, res) => {
-    console.log("Received Data:");
-    console.log(req.body);
-
-    res.json({ status: "success", message: "Data received" });
+app.get("/", (req, res) => {
+  res.send("Water Quality API Running");
 });
 
-app.get("/", (req, res) => {
-    res.send("Water Quality API Running");
+app.post("/api/data", (req, res) => {
+  console.log("Received Data:");
+  console.log(req.body);
+
+  res.json({
+    status: "success",
+    message: "Data received successfully"
+  });
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
